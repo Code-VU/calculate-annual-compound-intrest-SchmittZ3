c1829833b155502a63eb3e3c5f5dd266996ca275
Zach Schmitt <Zach.Schmitt@vu.com>
2022-02-22 21:44:41 -0600

    Schmitt - 4.5.3 Attempt 1

--- compoundInterest.py
+++ compoundInterest.py
@@ -2,9 +2,32 @@ def calculateCompoundInterest():
     
 # This first 3 lines are provided for yougetACompoundIntrest()
 # This first 3 lines are provided for you
- client_one_principal = float(input("Principle (amount): "))
- client_one_time =      float(input("Time:               "))
- client_one_rate =      float(input("Rate:               "))
+    client_one_principal = float(input("Principle (amount): "))
+    client_one_time =      float(input("Time:               "))
+    client_one_rate =      float(input("Rate:               "))
+    a = client_one_principal * ((1 + client_one_rate/100) ** client_one_time)
+    ci = a - client_one_principal
+    limited_float = round(ci, 2)
+    print('Compound Interest:',limited_float)
+    print('---')
+    client_one_principal = float(input("Principle (amount): "))
+    client_one_time =      float(input("Time:               "))
+    client_one_rate =      float(input("Rate:               "))
+    a = client_one_principal * ((1 + client_one_rate/100) ** client_one_time)
+    ci = a - client_one_principal
+    limited_float = round(ci, 2)
+    print('Compound Interest:',limited_float)
+    print('---')
+    client_one_principal = float(input("Principle (amount): "))
+    client_one_time =      float(input("Time:               "))
+    client_one_rate =      float(input("Rate:               "))
+    a = client_one_principal * ((1 + client_one_rate/100) ** client_one_time)
+    ci = a - client_one_principal
+    limited_float = round(ci, 2)
+    print('Compound Interest:',limited_float)
+
+calculateCompoundInterest()
+
  #print("Compound Interest: "+str(intrest))
 
     # end assignment
